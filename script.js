9d623be638ec1e381aed845471848c52748d16eb
JuniorDias1981 <139515193+JuniorDias1981@users.noreply.github.com>
2025-02-02 17:41:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('calcular').addEventListener('click', (e) => {
+    e.preventDefault();
+    const dataInicial = new Date(document.getElementById('data-inicial').value);
+    const dias = parseInt(document.getElementById('dias').value);
+    const dataFinal = new Date(dataInicial.getTime() + (dias + 1) * 24 * 60 * 60 * 1000);
+    const resultado = `Data da vacina: ${dataFinal.toLocaleDateString('pt-BR')}`;
+    document.getElementById('resultado').innerText = resultado;
+  });
